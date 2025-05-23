35a89df6fbc664a33cf10cbba1ee203f5137a264
James Harton <james@harton.nz>
2025-05-23 12:05:44 +1200

    chore(dev-deps): Unlock `earmark`.

--- mix.exs
+++ mix.exs
@@ -50,7 +50,6 @@ defmodule Kinemat.Mixfile do
       {:credo, "~> 1.6", only: ~w[dev test]a, runtime: false},
       {:dialyxir, "~> 1.4", only: ~w[dev test]a, runtime: false},
       {:doctor, "~> 0.22", only: ~w[dev test]a, runtime: false},
-      {:earmark, "~> 1.4", only: ~w[dev test]a, runtime: false},
       {:ex_check, "~> 0.16", only: ~w[dev test]a, runtime: false},
       {:ex_doc, "~> 0.30", only: ~w[dev test]a, runtime: false},
       {:git_ops, "~> 2.4", only: ~w[dev test]a, runtime: false},
